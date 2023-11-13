30456d1553b3e2a3ff35d3983bde7b0fb53a662e
Harika9500 <146800379+Harika9500@users.noreply.github.com>
2023-11-13 15:52:06 +0530

    state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (<div className="App">
       <h1>hello welcome to react</h1>
       <button>click me</button>
-      <Component comp={"1"}/>
+      <Component comp={"1"} user={{name :"harika",email:"harika903j@gmail.com",role:"developer"}}/>
 
       <SimpleState/>
     </div>
--- src/Component.jsx
+++ src/Component.jsx
@@ -1,11 +1,14 @@
 import react from 'react';
 function Component(props) {
-    console.log(props)
+    const{user,comp}=props
+   // console.log(props)
     return <div>
         <h1>{props.comp}</h1>
     <h1>inside component</h1>
     <h1>inside context</h1>
- 
+    <h4>name :{user.name}</h4>
+    <h4>role :{user.role}</h4>
+    <h4>email:{user.email}</h4>
     </div>
 }
 export default Component;
